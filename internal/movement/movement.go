7e7a2d862c745af44d884f6433819cbc79e97a54
R. Stephens Summerlin <st.summerlin@gmail.com>
2018-09-22 01:17:10 -0400

    Implemented SetCommand and unit test

--- internal/movement/movement.go
+++ internal/movement/movement.go
@@ -1,5 +1,10 @@
 package movement
 
+import (
+	fl "gonum.org/v1/gonum/floats"
+	"math"
+)
+
 type MoveDirProp struct {
 	MaxVelocity                      float64
 	Acceleration, Deceleration       float64
@@ -7,7 +12,7 @@ type MoveDirProp struct {
 }
 
 type MoveProp struct {
-	Forward, Backward, Left, Right MoveDirProp
+	Forward, Backward, Right, Left MoveDirProp
 	TurnRadiusRate, BaseTurnRadius float64
 }
 
@@ -18,8 +23,41 @@ type Movement struct {
 	CmdSpeed     float64    // Scaler representing desired movement speed
 }
 
-func (m *Movement) SetCommand(dir [2]float64, speed float64) {
+// Forward Backward, Right, Left
+func NewMovement(maxVel [4]float64) *Movement {
+	m := MoveProp{
+		Forward:  MoveDirProp{MaxVelocity: maxVel[0]},
+		Backward: MoveDirProp{MaxVelocity: maxVel[1]},
+		Right:    MoveDirProp{MaxVelocity: maxVel[2]},
+		Left:     MoveDirProp{MaxVelocity: maxVel[3]},
+	}
+	return &Movement{Properties: &m}
+}
 
+// dir[1] > 0 ? Front : Back; dir[0] > 0 ? Right : Left
+func (m *Movement) SetCommand(dir [2]float64, speed float64) {
+	// Set to unit vector if not already
+	fl.Scale(1/fl.Norm(dir[:], 1), dir[:])
+	copy(m.CmdDirection[:], dir[:])
+
+	// Check horizantal velocity
+	hsp := 0.0
+	if m.CmdDirection[0] > 0 {
+		hsp = math.Min(speed, m.Properties.Right.MaxVelocity) * m.CmdDirection[0]
+	} else {
+		hsp = math.Min(speed, m.Properties.Left.MaxVelocity) * m.CmdDirection[0] * -1
+	}
+
+	// Check vertical max velocity
+	vsp := 0.0
+	if m.CmdDirection[1] > 0 {
+		vsp = math.Min(speed, m.Properties.Forward.MaxVelocity) * m.CmdDirection[1]
+	} else {
+		vsp = math.Min(speed, m.Properties.Backward.MaxVelocity) * m.CmdDirection[1] * -1
+	}
+
+	// Combine
+	m.CmdSpeed = vsp + hsp
 }
 
 func (m *Movement) Update(del float64) float64 {
--- internal/movement/movement_test.go
+++ internal/movement/movement_test.go
@@ -1,1 +1,33 @@
 package movement
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSetCommand(t *testing.T) {
+	assert := assert.New(t)
+
+	tables := []struct {
+		dir      [2]float64
+		cmdSpeed float64
+		expDir   [2]float64
+		expSpeed float64
+	}{
+		{[2]float64{0, 1}, 4, [2]float64{0, 1}, 4},
+		{[2]float64{1, 0}, 4, [2]float64{1, 0}, 3},
+		{[2]float64{0, 2}, 8, [2]float64{0, 1}, 4},
+		{[2]float64{2, 0}, 8, [2]float64{1, 0}, 3},
+		{[2]float64{-1, 0}, 4, [2]float64{-1, 0}, 2},
+		{[2]float64{0, -1}, 4, [2]float64{0, -1}, 1},
+		{[2]float64{.5, .5}, 4, [2]float64{.5, .5}, 3.5},
+		{[2]float64{-.5, -.5}, 4, [2]float64{-.5, -.5}, 1.5},
+	}
+
+	for _, v := range tables {
+		m := NewMovement([4]float64{4, 1, 3, 2})
+		m.SetCommand(v.dir, v.cmdSpeed)
+		assert.Equal(v.expDir, m.CmdDirection)
+		assert.Equal(v.expSpeed, m.CmdSpeed)
+	}
+}
