7ff83f8e4c1c89af037369aa9e716ac34a7643e5
R. Stephens Summerlin <st.summerlin@gmail.com>
2018-09-23 16:23:41 -0400

    Wrote update unit-test

--- internal/movement/movement_test.go
+++ internal/movement/movement_test.go
@@ -60,3 +60,33 @@ func TestTurnRate(t *testing.T) {
 		assert.Equal(v.expTurnRadius, m.TurnRate())
 	}
 }
+
+func TestUpdate(t *testing.T) {
+	assert := assert.New(t)
+
+	tables := []struct {
+		dir      [2]float64
+		cmdSpeed float64
+		del      float64
+		upCount  int
+		expVel   [2]float64
+	}{
+		{[2]float64{0, 1}, 4, 0.01, 10, [2]float64{0, 0.4}}, // Forward, don't max
+		{[2]float64{0, 1}, 4, 0.1, 1, [2]float64{0, 0.4}},   // Forward, don't max, combine into 1 update
+		{[2]float64{0, 1}, 4, 0.1, 40, [2]float64{0, 4}},    // Forward, time to max speed
+		{[2]float64{0, 1}, 4, 0.2, 40, [2]float64{0, 4}},    // Forward, time beyond max
+		{[2]float64{0, 1}, 2, 0.1, 20, [2]float64{0, 2}},    // Forward half speed, time to max speed
+		{[2]float64{0, 1}, 2, 0.2, 20, [2]float64{0, 2}},    // Forward half speed, time beyond max
+	}
+
+	for _, v := range tables {
+		m := NewMovement([4]float64{4, 1, 3, 2}, [4]float64{4 / 4, 1 / 4, 3 / 4, 2 / 4}, [4]float64{4 / 8, 1 / 8, 3 / 8, 2 / 8}, [4]float64{}, [4]float64{}, 0, 0)
+		m.SetCommand(v.dir, v.cmdSpeed)
+
+		for i := 0; i < v.upCount; i++ {
+			m.Update(v.del)
+		}
+
+		assert.Equal(v.expVel, m.CurVelocity)
+	}
+}
