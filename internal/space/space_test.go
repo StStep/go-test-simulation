c49c3d8ccc0f777f1f8b00d716af067a83bb3a60
R. Stephens Summerlin <st.summerlin@gmail.com>
2018-09-29 22:09:16 -0400

    implemented Space.UpdateEntity test

--- internal/space/space_test.go
+++ internal/space/space_test.go
@@ -22,10 +22,43 @@ func TestRegisteration(t *testing.T) {
 	for i, v := range tables {
 		s := NewSpace()
 		for k := 0; k < v.count; k++ {
-			assert.Equal(k+1, s.RegisterEntity(v.pos, v.radius), "Test %v", i)
-			assert.True(s.UpdateEntity(k+1, v.vel))
+			assert.Equalf(k+1, s.RegisterEntity(v.pos, v.radius), "Test %v", i)
+			assert.Truef(s.UpdateEntity(k+1, v.vel), "Test %v", i)
 		}
-		assert.Equal(v.expCount, s.EntityCount(), "Test %v", i)
+		assert.Equalf(v.expCount, s.EntityCount(), "Test %v", i)
+
+		for k := 0; k < v.count; k++ {
+			s.UnregisterEntity(k + 1)
+		}
+		assert.Equalf(0, s.EntityCount(), "Test %v", i)
+	}
+}
+
+func TestUpdateEntity(t *testing.T) {
+	assert := assert.New(t)
+
+	tables := []struct {
+		count int
+		id    int
+		vel   [2]float64
+		exp   bool
+	}{
+		{4, 1, [2]float64{2, 5}, true},  // Standard usage
+		{4, 5, [2]float64{1, 2}, false}, // Unknown ID error
+	}
+
+	for i, v := range tables {
+		s := NewSpace()
+		for k := 0; k < v.count; k++ {
+			assert.Equalf(k+1, s.RegisterEntity([2]float64{}, 0), "Test %v", i)
+		}
+		if v.exp {
+			assert.Truef(s.UpdateEntity(v.id, v.vel), "Test %v", i)
+			vel := s.velocity[v.id]
+			assert.InDeltaSlicef(vel[:], v.vel[:], 0.01, "Test %v", i)
+		} else {
+			assert.False(s.UpdateEntity(v.id, v.vel), "Test %v", i)
+		}
 
 		for k := 0; k < v.count; k++ {
 			s.UnregisterEntity(k + 1)
