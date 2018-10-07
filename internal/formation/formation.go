553290373459dd6ceb6adfda6269bb24dba638a3
R. Stephens Summerlin <st.summerlin@gmail.com>
2018-10-07 02:28:18 -0400

    Implemented TakClosestSlot and slot take/give methods

--- internal/formation/formation.go
+++ internal/formation/formation.go
@@ -1,6 +1,10 @@
 package formation
 
-import ()
+import (
+	"fmt"
+	fl "gonum.org/v1/gonum/floats"
+	"strings"
+)
 
 type FormationProp struct {
 	style       string
@@ -49,18 +53,59 @@ func (f *Formation) Resize(size int) {
 				f.tags[i] = "Captain"
 				f.slots[i] = [2]float64{0.0, 0.0}
 			} else {
-				f.tags[i] = "grunt"
+				f.tags[i] = "Grunt"
 				fInd := i % f.Prop.width
 				rInd := i / f.Prop.width
 				f.slots[i] = [2]float64{float64(fInd-hwidth) * f.Prop.fileSpacing, float64(hrank-rInd) * f.Prop.rankSpacing}
 			}
-			f.isEmpty[i] = false
+			f.isEmpty[i] = true
 		}
 	} else {
 		panic("Not implemented") // TODO
 	}
 }
 
-func (f *Formation) ClosestOpenSlot(tag string, rpos [2]float64) int {
-	return -1
+func (f *Formation) TakeClosestOpenSlot(tag string, rpos [2]float64) (int, bool) {
+	minSlot := -1
+	minDist := 0.0
+	for i := 0; i < f.size; i++ {
+		if f.isEmpty[i] && f.tags[i] == tag {
+			slot := f.slots[i]
+			dist := fl.Distance(rpos[:], slot[:], 2)
+			if minSlot == -1 || dist < minDist {
+				minSlot = i
+				minDist = dist
+			}
+		}
+	}
+	return minSlot, minSlot != -1
+}
+
+func (f *Formation) TakeSlot(i int) bool {
+	if f.isEmpty[i] {
+		f.isEmpty[i] = false
+		return true
+	} else {
+		return false
+	}
+}
+
+func (f *Formation) GiveSlot(i int) {
+	f.isEmpty[i] = true
+}
+
+func (f *Formation) Offset(i int) [2]float64 {
+	return f.slots[i]
+}
+
+func (f *Formation) DebugOffsets() string {
+	rows := make([]string, 0, f.size+f.size/f.Prop.width) // one per unit and per end-of-rank for \n
+	for i, v := range f.slots {
+		rows = append(rows, fmt.Sprint(v)+"\t")
+		// Append newline at end-of-ranks
+		if i%f.Prop.width == f.Prop.width-1 {
+			rows = append(rows, "\n")
+		}
+	}
+	return strings.Join(rows, "")
 }
--- internal/formation/formation_test.go
+++ internal/formation/formation_test.go
@@ -18,6 +18,7 @@ func TestNewFormation(t *testing.T) {
 		firstOffset [2]float64
 	}{
 		{"", 5, .25, .25, 20, 7, [2]float64{-0.5, 0.25}}, // Basic 5 file 4 rank block
+		{"", 1, .25, .25, 1, 0, [2]float64{0, 0}},        // Single unit
 	}
 
 	for i, v := range tables {
@@ -29,5 +30,41 @@ func TestNewFormation(t *testing.T) {
 			assert.InDeltaf(v.firstOffset[0]+float64(k%v.width)*v.fileSpacing, f.slots[k][0], 0.01, "Test %v, 0 Loop %v", i, k)
 			assert.InDeltaf(v.firstOffset[1]-float64(k/v.width)*v.rankSpacing, f.slots[k][1], 0.01, "Test %v, 1 Loop %v", i, k)
 		}
+	}
+}
+
+func TestGiveTakeSlot(t *testing.T) {
+	// TODO
+}
+
+func TestTakeClosestOpenSlot(t *testing.T) {
+	assert := assert.New(t)
+
+	tables := []struct {
+		style       string
+		width       int
+		fileSpacing float64
+		rankSpacing float64
+		size        int
+		pos         [2]float64
+		tag         string
+		notEmpty    []int
+		expSlot     int
+		expFound    bool
+	}{
+		{"", 5, .25, .25, 20, [2]float64{0.0, 0.0}, "Captain", []int{0, 1, 2, 3}, 7, true}, // Empty Captain slot Basic block
+		{"", 5, .25, .25, 20, [2]float64{0.0, 0.0}, "Captain", []int{7}, -1, false},        // Full Captain slot Basic block
+		{"", 5, .25, .25, 20, [2]float64{-0.1, 0.25}, "Grunt", []int{3}, 2, true},          // Grunt slot Basic block
+		{"", 5, .25, .25, 20, [2]float64{-0.1, 0.25}, "Grunt", []int{2}, 1, true},          // Full Grunt slot Basic block
+	}
+
+	for i, v := range tables {
+		f := NewFormation(v.style, v.width, v.fileSpacing, v.rankSpacing, v.size)
+		for _, closedSlot := range v.notEmpty {
+			assert.Truef(f.TakeSlot(closedSlot), "Test %v", i)
+		}
+		slot, isFound := f.TakeClosestOpenSlot(v.tag, v.pos)
+		assert.Equalf(v.expSlot, slot, "Test %v", i)
+		assert.Equalf(v.expFound, isFound, "Test %v", i)
 	}
 }
